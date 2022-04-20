a9f54e65bbe5aa254d2fdd9e7e72129e97ffe984
Gustavo-Alegre <gustavo.alegre04@hotmail.com>
2022-04-19 21:21:16 -0300

    carga2

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -3,10 +3,10 @@ const { Router } = require('express');
 // Ejemplo: const authRouter = require('./auth.js');
 
 const pokemons = require('./pokemons');
-const pokemonsPost = require('./pokemonPost');
-const pokemonsType = require('./pokemonType');
-const pokemonsId = require('./pokemonsId');
-const pokemonsDelete = require('./pokemonDelete');
+const pokemonspost = require('./pokemonpost');
+const pokemonstype = require('./pokemontype');
+const pokemonsid = require('./pokemonsid');
+const pokemonsdelete = require('./pokemondelete');
 
 const router = Router();
 
@@ -14,10 +14,10 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 
 router.use('/pokemons' , pokemons);
-router.use('/pokemons' ,  pokemonsPost)
-router.use('/types' , pokemonsType)
-router.use('/pokemons' , pokemonsId)
-router.use('/pokemons/delete' , pokemonsDelete)
+router.use('/pokemons' ,  pokemonspost)
+router.use('/types' , pokemonstype)
+router.use('/pokemons' , pokemonsid)
+router.use('/pokemons/delete' , pokemonsdelete)
 
 module.exports = router;
 
